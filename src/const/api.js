cacded88c3cac869565ffa708ed39d59e9db30ad
coder <coder@coders-MacBook-Pro.local>
2021-01-25 09:30:13 +0100

    fixed routes

--- src/const/api.js
+++ src/const/api.js
@@ -1,7 +1,5 @@
-export const REACT_APP_API = process.env.REACT_APP_API
-
 export const API_PROTOCOL = process.env.REACT_APP_API_PROTOCOL
 export const API_DOMAIN = process.env.REACT_APP_API_DOMAIN
 export const API_PATH = process.env.REACT_APP_API_PATH
 
-export const API_URL=`${API_PROTOCOL}://${API_DOMAIN}/${API_PATH}`
+export const API_URL = `${API_PROTOCOL}://${API_DOMAIN}/${API_PATH}`
